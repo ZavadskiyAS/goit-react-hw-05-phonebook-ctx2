43fd827afc5a2acaa0f646ec95ed92b5072232fe
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-07-24 15:39:18 +0300

    Updates

---
+++ precache-manifest.0f710e0bf48b198bd3351ceba037ed90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b924919fab0f91a57daad615f07e0f56",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/index.html"
+  },
+  {
+    "revision": "3a1a9a9c57cc4b2bd73e",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/static/css/main.2dfd7847.chunk.css"
+  },
+  {
+    "revision": "238c985a0516c864b9dd",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/static/js/2.ae7a00b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/static/js/2.ae7a00b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1a9a9c57cc4b2bd73e",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/static/js/main.dd8a6e0b.chunk.js"
+  },
+  {
+    "revision": "36e169f5b78fc8e0be11",
+    "url": "/ZavadskiyAS.github.io/goit-react-hw-05-phonebook-ctx2/static/js/runtime-main.89dda255.js"
+  }
+]);
